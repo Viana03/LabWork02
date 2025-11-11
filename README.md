02f81433deed15f0018ad3646dbf1355584c98ef
DiogoSSMartins <diogossilvamartins@ua.pt>
2025-11-11 02:41:38 +0000

    ex1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LabWork02
+
+# ex1 - ../bin/extract_channel ../images/arial.ppm ../output/arialC1.ppm 1
---
+++ src/extract_channel.cpp
@@ -0,0 +1,48 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <input_image> <output_image> <channel_number>\n";
+        cerr << "Channel number: 0 = Blue, 1 = Green, 2 = Red\n";
+        return 1;
+    }
+
+    string inputFile = argv[1];
+    string outputFile = argv[2];
+    int channel = stoi(argv[3]);
+
+    if (channel < 0 || channel > 2) {
+        cerr << "Error: channel number must be 0, 1, or 2\n";
+        return 1;
+    }
+
+    Mat image = imread(inputFile, IMREAD_COLOR);
+    if (image.empty()) {
+        cerr << "Error: cannot open image file " << inputFile << endl;
+        return 1;
+    }
+
+    Mat output(image.rows, image.cols, CV_8UC3, Scalar(0, 0, 0));
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            Vec3b pixel = image.at<Vec3b>(i, j);
+            Vec3b outPixel(0, 0, 0);
+            outPixel[channel] = pixel[channel];
+            output.at<Vec3b>(i, j) = outPixel;
+        }
+    }
+
+    if (!imwrite(outputFile, output)) {
+        cerr << "Error: could not save output image to " << outputFile << endl;
+        return 1;
+    }
+
+    cout << "Extracted channel " << channel << " from " << inputFile
+         << " and saved result to " << outputFile << endl;
+
+    return 0;
+}
